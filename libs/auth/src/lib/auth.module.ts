f29c487dc8af9ae03958e1e2da0b7cec66e0fc68
bab2683 <barahona.braulio@gmail.com>
2019-09-09 12:06:10 +0100

    tslint and base libraries porting

---
+++ libs/auth/src/lib/auth.module.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+
+import { AUTH_STATE, AuthEffects, authReducer } from './store';
+
+@NgModule({
+  imports: [
+    AngularFireAuthModule,
+    CommonModule,
+    EffectsModule.forFeature([AuthEffects]),
+    StoreModule.forFeature(AUTH_STATE, authReducer)
+  ]
+})
+export class AuthModule {}
---
+++ libs/auth/src/lib/store/auth.effects.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { FirebaseError } from 'firebase';
+import { from, Observable, of } from 'rxjs';
+import { catchError, first, map, switchMap } from 'rxjs/operators';
+
+import { getRelevantUserData, parseLoginError } from '../utils';
+import {
+  AuthActionTypes,
+  AuthInitCheck,
+  AuthLoadedAction,
+  AuthLoginFail,
+  AuthLoginRequest,
+  AuthLoginSuccess,
+  AuthLogoutFail,
+  AuthLogoutRequest,
+  AuthLogoutSuccess
+} from './auth.actions';
+
+@Injectable()
+export class AuthEffects {
+  @Effect()
+  public initAuth$: Observable<AuthLoadedAction> = this.actions$.pipe(
+    ofType<AuthInitCheck>(AuthActionTypes.INIT_CHECK),
+    switchMap(() => {
+      return this.afAuth.user.pipe(
+        first(),
+        switchMap(user => {
+          return of(
+            new AuthLoadedAction(user ? getRelevantUserData(user) : null)
+          );
+        })
+      );
+    })
+  );
+
+  @Effect()
+  public loginRequest$: Observable<
+    AuthLoginFail | AuthLoginSuccess
+  > = this.actions$.pipe(
+    ofType<AuthLoginRequest>(AuthActionTypes.LOGIN_REQUEST),
+    switchMap(({ payload: { email, password } }) =>
+      from(this.afAuth.auth.signInWithEmailAndPassword(email, password)).pipe(
+        switchMap(({ user }) => {
+          return of(new AuthLoginSuccess(getRelevantUserData(user)));
+        }),
+        catchError((err: FirebaseError) => {
+          return of(new AuthLoginFail(parseLoginError(err)));
+        })
+      )
+    )
+  );
+
+  @Effect()
+  public logoutRequest$: Observable<
+    AuthLogoutSuccess | AuthLogoutFail
+  > = this.actions$.pipe(
+    ofType<AuthLogoutRequest>(AuthActionTypes.LOGOUT_REQUEST),
+    switchMap(() =>
+      from(this.afAuth.auth.signOut()).pipe(
+        map(() => new AuthLogoutSuccess()),
+        catchError((err: any) => of(new AuthLogoutFail({ message: err.err })))
+      )
+    )
+  );
+
+  constructor(private actions$: Actions, private afAuth: AngularFireAuth) {}
+}
