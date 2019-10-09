5f7c1f7ae7f859acd472dc56a2785f87aa06f3e5
bab2683 <barahona.braulio@gmail.com>
2019-10-09 09:46:00 +0100

    feat: v24

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,16 +1,13 @@
-const { execSync } = require('child_process');
-
 const { getDeployableLibs, getPublishType } = require('./affected');
 const { deployLib } = require('./publish-lib');
 
-const { getTagForRelease } = require('./tagging');
-
+const branch = process.argv.splice(2)[0];
 const type = getPublishType();
 
 if (type) {
   console.log(`${type} publish started`);
 
-  const deployableLibs = getDeployableLibs();
+  const deployableLibs = getDeployableLibs(branch);
 
   if (deployableLibs.length) {
     console.log('deployable libs', deployableLibs);
@@ -20,17 +17,6 @@ if (type) {
       deployLib(lib, type);
     });
     console.log(`${type} publish finished`);
-
-    console.log('creating tag');
-
-    const tag = getTagForRelease(deployableLibs);
-
-    execSync(`git tag -a ${tag}`);
-
-    execSync(
-      'git add -A && git push origin HEAD:master && git push origin HEAD:master --tags'
-    );
-    console.log('tags pushed to repo');
   } else {
     console.log('no deployable libs');
   }
