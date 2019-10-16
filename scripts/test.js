a3ebea50622c957649fec9aa51e46d267d58dd8c
bab2683 <barahona.braulio@gmail.com>
2019-10-16 08:36:22 +0100

    test save v6

--- scripts/test.js
+++ scripts/test.js
@@ -1,2 +1,9 @@
-const branch = process.argv.splice(2)[0];
-console.log('branch', branch);
+const { execSync } = require('child_process');
+
+const result = execSync(
+  `git http.extraheader="AUTHORIZATION: token ${
+    process.env.GITHUB_AUTH_TOKEN
+  }" push`
+);
+
+console.log('result', result);
